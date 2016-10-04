ed3c1f69b83fffca7f4fd792feb95536ef0a7608
mpastorperez <mpastorperez@atanasoff31.danielcastelao.org>
2016-10-04 09:05:53 +0200

    sexto ejercicio del boletin 2

---
+++ src/boletin2_6/Boletin2_6.java
@@ -0,0 +1,18 @@
+
+package boletin2_6;
+
+import javax.swing.JOptionPane;
+
+
+public class Boletin2_6 {
+
+    
+    public static void main(String[] args) {
+        float prezoTarifa, prezoPagado, porcentaxeDescontada;
+        prezoTarifa= Float.parseFloat(JOptionPane.showInputDialog("prezoTarifa"));
+        prezoPagado= Float.parseFloat(JOptionPane.showInputDialog("prezoPagado"));
+        porcentaxeDescontada=100*prezoTarifa-prezoPagado/prezoTarifa;
+        JOptionPane.showMessageDialog(null,"A porcentaxe descontada=" +porcentaxeDescontada);
+    }
+    
+}
